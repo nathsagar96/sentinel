3881edf00e5b1912f285226f8463190df664cfce
Sagar Nath <nathsagar96@gmail.com>
2026-07-26 09:57:30 +0530

    chore: minor cleanup and ddl-auto update

--- backend/src/main/java/com/sentinel/config/SecurityConfig.java
+++ backend/src/main/java/com/sentinel/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import com.sentinel.oauth2.OAuth2AuthenticationSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
